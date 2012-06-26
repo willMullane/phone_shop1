21d7652904cac1577b1ba09b4d1c6301259558ad
willMullane <will-mull@hotmail.com>
2012-06-26 18:56:17 +0100

    Initial Commit
    This version of the project is based on the Laptopshop tutorials 1-4

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+require 'digest'
+class User < ActiveRecord::Base
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return user if user && user.authenticated?(password)
+  end
+  def authenticated?(password)
+    self.hashed_password == encrypt(password)
+  end
+
+  validates :email,
+            :uniqueness => true,
+            :length => {:within => 5..50},
+            :format => {:with =>
+                            /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i}
+  validates :password,
+            :confirmation => true,
+            :length => {:within => 4..20},
+            :presence => true
+
+  has_many :products
+  attr_accessor :password
+  before_save :encrypt_password
+  protected
+  def encrypt_password
+    return if password.blank?
+    self.hashed_password = encrypt(password)
+  end
+  def encrypt(string)
+    Digest::SHA1.hexdigest(string)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaptopShop1::Application.config.secret_token = 'fc327ba2b325b5083a76c023e1ea74466fb3546996dd2a3522d52aacb7646f3ea78aa3bc5733088241f7f7aa7707cda98366c870637419cc220285157a26d840'
